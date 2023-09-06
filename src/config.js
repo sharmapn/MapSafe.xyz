05a280b589cc58a2c4b2d4529d3ace436ea4309d
PankajSharma <pankajeshwara@gmail.com>
2023-09-06 12:51:34 +1200

    Delete src directory

--- src/config.js
+++
@@ -1,3 +0,0 @@
-//export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-const fromAddress = "0x244EAbEf05ACF009746Ce91fE1712Daf3857e620"
-const nftAddress = "0xa505630672898f901A0c6B261A693C39f17e66B8"
